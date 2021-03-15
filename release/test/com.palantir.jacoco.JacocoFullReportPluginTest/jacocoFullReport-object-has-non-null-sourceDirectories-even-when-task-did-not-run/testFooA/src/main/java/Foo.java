715ed8691645035d718ecec678f46f90e715c697
sanjay singh <sanjay.singh.gvskt002@gmail.com>
2021-03-15 20:48:10 +0000

    add release

---
+++ release/test/com.palantir.jacoco.JacocoFullReportPluginTest/jacocoFullReport-object-has-non-null-sourceDirectories-even-when-task-did-not-run/testFooA/src/main/java/Foo.java
@@ -0,0 +1,16 @@
+
+public class Foo {
+    public int fooA() {
+        int variable = 1;
+        if (variable == 1) {
+            variable = 2;
+        }
+
+        return variable;
+    }
+
+    public int fooB() {
+        int variable = 2;
+        return variable;
+    }
+}
