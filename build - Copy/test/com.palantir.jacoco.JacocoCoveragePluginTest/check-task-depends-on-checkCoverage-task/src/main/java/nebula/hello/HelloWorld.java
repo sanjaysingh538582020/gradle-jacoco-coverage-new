207d1d576f0da9184b9c71408e31d483e77b4215
sanjay singh <sanjay.singh.gvskt002@gmail.com>
2021-03-15 20:31:17 +0000

    add build

---
+++ build - Copy/test/com.palantir.jacoco.JacocoCoveragePluginTest/check-task-depends-on-checkCoverage-task/src/main/java/nebula/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package nebula.hello;
+
+            public class HelloWorld {
+                public static void main(String[] args) {
+                    System.out.println("Hello Integration Test");
+                }
+            }
+        
---
+++ build - Copy/test/com.palantir.jacoco.JacocoFullReportPluginTest/jacoco-full-report-can-exclude-sub-projects/testFooB/src/test/java/FooBTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.Test;
+
+public class FooBTest {
+    @Test
+    public void testFooB() {
+        Foo foo = new Foo();
+        foo.fooB();
+    }
+}
