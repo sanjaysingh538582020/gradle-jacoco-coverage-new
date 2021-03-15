c855bd14ea3bc0dd76eaabfaedd2af5f36a6abb9
sanjaysingh538582020 <71742124+sanjaysingh538582020@users.noreply.github.com>
2021-03-15 22:53:01 +0000

    delete

--- build - Copy/test/com.palantir.jacoco.JacocoCoveragePluginTest/All-syntax-variations-work/src/main/java/nebula/hello/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package nebula.hello;
-
-            public class HelloWorld {
-                public static void main(String[] args) {
-                    System.out.println("Hello Integration Test");
-                }
-            }
-        
